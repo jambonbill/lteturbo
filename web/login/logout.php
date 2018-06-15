2bc6fee0bd10a92734826172e54392d60acc750e
jambonbill <jambonbill@gmail.com>
2018-06-15 17:34:30 +0800

    web/login

---
+++ web/login/logout.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+session_start();
+
+require __DIR__ . "/../../vendor/autoload.php";
+
+$admin = new LTE\Admin;
+$admin->title("Login");
+$admin->config()->menu = (object)[];//unset the global menu
+$admin->config()->layout->{'sidebar-collapse'}=true;
+echo $admin->head();
+?>
+<pre>your logout script here</pre>
