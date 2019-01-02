8661c06a7d6e612141fc80da149b9de73f083320
orange <jambonbill@gmail.com>
2019-01-02 17:39:39 +0800

    new login page

--- web/login/index.php
+++ web/login/index.php
@@ -6,72 +6,53 @@ session_start();
 require __DIR__ . "/../../vendor/autoload.php";
 
 $admin = new LTE\Admin(__DIR__."/../../config/config.json");
-//$admin->title("Login");
-//$admin->config()->menu = (object)[];//unset the global menu
-//$admin->config()->layout->{'sidebar-collapse'}=true;
 echo $admin->head();
 ?>
 
-<body class="text-center">
-    <form class="form-signin" method="post" action="login.php" autocomplete="off">
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" name=email id="inputEmail" class="form-control" 
-      placeholder="Email address" required autofocus autocomplete="off">
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" name="password" id="inputPassword" class="form-control" 
-      placeholder="Password" required autocomplete="off">
-      
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    </form>
+<body class="hold-transition login-page">
+
+  <div class="login-box">
+
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+
+      <form action="login.php" method="post" autocomplete="off">
+        <div class="input-group mb-3">
+          <input type="email" name=email id="inputEmail" class="form-control" placeholder="Email" autocomplete="off">
+          <div class="input-group-append">
+              <span class="fa fa-envelope input-group-text"></span>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" name=password id="inputPassword" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+              <span class="fa fa-lock input-group-text"></span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+
+
+      <p class="mb-1">
+        <a href="#">I forgot my password</a>
+      </p>
+
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+
+<script type="text/javascript">
+  document.getElementById('inputEmail').focus();
+</script>
 </body>
-
-<style type="text/css">
-html,
-body {
-  height: 100%;
-}
-
-body {
-  display: -ms-flexbox;
-  display: -webkit-box;
-  display: flex;
-  -ms-flex-align: center;
-  -ms-flex-pack: center;
-  -webkit-box-align: center;
-  align-items: center;
-  -webkit-box-pack: center;
-  justify-content: center;
-  padding-top: 40px;
-  padding-bottom: 40px;
-  background-color: #f5f5f5;
-}
-
-.form-signin {
-  width: 100%;
-  max-width: 330px;
-  padding: 15px;
-  margin: 0 auto;
-}
-
-.form-signin .form-control {
-  position: relative;
-  box-sizing: border-box;
-  height: auto;
-  padding: 10px;
-  font-size: 16px;
-}
-.form-signin .form-control:focus {
-  z-index: 2;
-}
-.form-signin input[type="email"] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
-}
-.form-signin input[type="password"] {
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-}
-</style>
--- web/login/login.php
+++ web/login/login.php
@@ -4,19 +4,19 @@ session_start();
 
 require __DIR__ . "/../../vendor/autoload.php";
 
-//exit("hi");
+exit("hi");
 //print_r($_POST);
 // Login logic//
 
 $B=new CRM\Base;
 $UD=new Django\UserDjango($B->db());
 
-if($UD->login($_POST['email'], $_POST['password'])){
+if ($UD->login($_POST['email'], $_POST['password'])) {
     //echo "ok";
     //echo '<script>document.location.href="../home";</script>';
     header('location:../home');
     exit;
-}else{
+} else {
     echo "nope";
     echo '<script>document.location.href="index.php";</script>';
 }
