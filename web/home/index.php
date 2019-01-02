1d71d03a4481aea4312f25ffcd43323674817bab
orange <jambonbill@gmail.com>
2019-01-02 17:39:47 +0800

    -$B=new CRM\Base;

--- web/home/index.php
+++ web/home/index.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ *
+ */
 header('Content-Type: text/html; charset=utf-8');
 session_start();
 
@@ -10,12 +12,11 @@ $admin->configfile(__DIR__."/../../config/config.json");
 //$admin->description("Testing the tests is my hobby");
 echo $admin;
 
-$B=new CRM\Base;
 
 ?>
 <div class="content-wrapper">
 
-	<!-- Content Header (Page header) -->
+    <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
@@ -26,47 +27,47 @@ $B=new CRM\Base;
       </div><!-- /.container-fluid -->
     </section>
 
-	<!--Main content-->
-	<section class="container">
+    <!--Main content-->
+    <section class="container">
 
-		<div class="row">
+        <div class="row">
 
-			<div class="col-12">
-				<?php
+            <div class="col-12">
+                <?php
 
-				$C=new LTE\Callout("warning","Callout","This is a new LTE\Callout");
-				echo $C;
+                $C=new LTE\Callout("warning", "Callout", "This is a new LTE\Callout");
+                echo $C;
 
-				$A=new LTE\Alert("info","Alert","I'm a new LTE\Alert and i can be removed");
-				echo $A;
-				?>
-			</div>
+                $A=new LTE\Alert("info", "Alert", "I'm a new LTE\Alert and i can be removed");
+                echo $A;
+                ?>
+            </div>
 
-			<div class="col-md-12">
-				<?php
+            <div class="col-md-12">
+                <?php
 
-				$tools='<div class="has-feedback">';
-	            $tools.='<input class="form-control form-control-sm" placeholder="Search ..." type="text">';
-	            $tools.='<span class="glyphicon glyphicon-search form-control-feedback"></span>';
-	            $tools.='</div>';
+                $tools='<div class="has-feedback">';
+                $tools.='<input class="form-control form-control-sm" placeholder="Search ..." type="text">';
+                $tools.='<span class="glyphicon glyphicon-search form-control-feedback"></span>';
+                $tools.='</div>';
 
-	            $htm='<pre>'.print_r($B->user(),1).'</pre>';
+                $htm='<pre>hi</pre>';
 
-				$box=new LTE\Card;
-				$box->title("Card");
-				//$box->tools($tools);
-				$box->body($htm);
-				$box->removable(1);
-				$box->collapsable(1);
-				echo $box;
+                $box=new LTE\Card;
+                $box->title("Card");
+                //$box->tools($tools);
+                $box->body($htm);
+                $box->removable(1);
+                $box->collapsable(1);
+                echo $box;
 
-				//echo '<pre>';print_r($_SESSION);echo '</pre>';
-				?>
-			</div>
+                //echo '<pre>';print_r($_SESSION);echo '</pre>';
+                ?>
+            </div>
 
-		</div>
+        </div>
 
-	</section>
+    </section>
 </div>
 <?php
 $admin->end();
