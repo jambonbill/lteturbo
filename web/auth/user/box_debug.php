acf53b5ef02d00761ef8577db91fee26f5852232
orange <jambonbill@gmail.com>
2020-11-19 10:47:18 +0100

    gone

--- web/auth/user/box_debug.php
+++
@@ -1,9 +0,0 @@
-<?php
-//user info
-
-$box=new LTE\Card;
-//$box->title("User #".$user['id']);
-//$box->icon("fa fa-edit");
-$box->body("<pre>".print_r($user,true)."</pre>");
-$box->removable(1);
-echo $box;
